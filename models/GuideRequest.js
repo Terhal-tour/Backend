1bb720fcd0deac1304aba649378f2073914e6b86
Mariam Elbaz <maruoma.we.bas@gmail.com>
2025-07-24 12:23:05 +0300

    add guide payment

--- models/GuideRequest.js
+++ models/GuideRequest.js
@@ -1,59 +1,62 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const guideRequestSchema = new mongoose.Schema({
-    userName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    userEmail: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true
-    },
-    message: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        enum: ['pending', 'approved', 'rejected'],
-        default: 'pending'
-    },
-    date: {
-        type: Date,
-        required: true
-    },
-    duration: {
-        type: String,
-        required: true
-    },
-    groupSize: {
-        type: Number,
-        required: true,
-        min: 1
-    },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        // required: true
-    },
-    guideId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        // required: true
-    },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    guide: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  userName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  userEmail: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "approved", "rejected"],
+    default: "pending",
+  },
+  paid: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  groupSize: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    // required: true
+  },
+  guideId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    // required: true
+  },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  guide: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 
-
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const GuideRequest = mongoose.model('GuideRequest', guideRequestSchema);
+const GuideRequest = mongoose.model("GuideRequest", guideRequestSchema);
 
 export default GuideRequest;
