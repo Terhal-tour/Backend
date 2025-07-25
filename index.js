c2dca117ffc8506bf3d54f84c3ff84d36ec215b9
SafaaMagdyAwad <engsafaamagdy1024@gmail.com>
2025-07-25 21:08:18 +0300

    coflict

--- index.js
+++ index.js
@@ -34,8 +34,7 @@ import { guideRouter } from "./routes/GuideRouter.js";
 import { guideRequestRouter } from "./routes/GuideRequestRouter.js";
 import paymentRoutes from "./routes/paymentRoutes.js"
 import supportusRoutes  from "./routes/supportusRoutes.js"
-import paymentRoutes from "./routes/paymentRoutes.js"
-import supportusRoutes  from "./routes/supportusRoutes.js"
+
 
 import postRoutes from './routes/user-interactions/postRoutes.js';
 import commentsRoutes from './routes/user-interactions/commentRoutes.js';
@@ -90,14 +89,6 @@ app.use("/user/reviews", reviewRoutes);
 app.use("/randomplaces",randomPlaceRoute)
 // payment
 app.use("/payments", paymentRoutes);
-
-
-// payment
-app.use("/payments", paymentRoutes);
-
-// support us routes
-app.use("/supportus", supportusRoutes);
-
 
 // support us routes
 app.use("/supportus", supportusRoutes);
