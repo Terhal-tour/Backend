afe668f8c2f4bedbe0a4c4672734096d1e5af935
SafaaMagdyAwad <engsafaamagdy1024@gmail.com>
2025-07-26 21:29:18 +0300

    some fixing

--- services/guidRequestService.js
+++ services/guidRequestService.js
@@ -1,5 +1,8 @@
 import GuideRequest from '../models/GuideRequest.js';
 import User from '../models/User.js';
+import mongoose from 'mongoose';
+
+const { ObjectId } = mongoose.Types;
 
 export const createGuideRequestService = async (guideId, userId, data) => {
     const { message, date, duration } = data;
@@ -58,7 +61,12 @@ export const getRequestsByGuideService = async (guideId) => {
 export const updateRequestStatusService = async (requestId, guideId, status, price) => {
     const request = await GuideRequest.findById(requestId);
     if (!request) throw new Error('Request not found');
-    if (!request.guide.equals(guideId)) {
+    
+    const guideObjectId = typeof guideId === 'string' ? new ObjectId(guideId) : guideId;
+
+    console.log('Comparing:', request.guide.toString(), guideObjectId.toString());
+
+    if (!request.guide.equals(guideObjectId)) {
         throw new Error('Not authorized to modify this request');
     }
 
